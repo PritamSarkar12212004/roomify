1bb79fbeee7e8560159685a7d830f951c6156f46
pritam sarkar <pritam7796419792@gmail.com>
2024-08-25 18:13:12 +0530

    aok

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(
-  {
-    server: {
-      watch: [{ usePolling: true }],
-    },
-  },
-  {
-    plugins: [react()],
-  }
-);
+export default defineConfig({
+  plugins: [react()],
+})
