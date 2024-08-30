d53d4004afcf08a1ecc5d5bcdb5d6e99f1bfae64
pritam sarkar <pritam7796419792@gmail.com>
2024-08-31 00:47:46 +0530

    ge

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://roomify-backend.vercel.app/",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-    },
-  },
   plugins: [react()],
-});
+})
