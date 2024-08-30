8ae8ea3d81d9cc97d4a944fec96ee52bf158abbb
pritam sarkar <pritam7796419792@gmail.com>
2024-08-31 00:53:45 +0530

    efef

--- src/utils/api/axios.js
+++ src/utils/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "https://roomify-backend.vercel.app/",
+  baseURL: "https://roomify-backend.vercel.app/" || "http://localhost:3000/",
 });
 export default axiosInstance;
