dcbef5e36286acd128270b1c176583377ac916e1
PritamSarkar12212004 <pritamsarkar21122004@gmail.com>
2024-10-23 21:05:52 +0530

    all set

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Proxy all requests to your backend server
+      "/": {
+        target: "https://frotify-backend.vercel.app", // Replace with your backend URL
+        changeOrigin: true,
+      },
+    },
+  },
+});
